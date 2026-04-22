a193502be644aaccdfe240e71e91a061c71d1ab3
moham <moham@Dalal>
2026-04-22 21:18:54 +0300

    update 1 22/4/2026

--- Course_Registration_System/src/Student.java
+++ Course_Registration_System/src/Student.java
@@ -1,5 +1,18 @@
-
 public class Student {
 
-	// test halat
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+
+    @Override
+    public String toString() {
+        return "Student{id=" + id + ", name='" + name + "'}";
+    }
 }
